f98ebda95cc204627fe6d868814a2a3c95e8ff2b
jxfzzzt <1160490625@qq.com>
2026-02-02 14:03:06 +0800

    init webflux project

--- src/main/java/edu/fdu/se/webflux/demo/controller/UserController.java
+++ src/main/java/edu/fdu/se/webflux/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package edu.fdu.se.webflux.demo.controller;
 
 import edu.fdu.se.webflux.demo.domain.User;
 import edu.fdu.se.webflux.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
+@Tag(name = "用户服务")
 @RequestMapping("/api/user")
 @RestController
 public class UserController {
@@ -17,11 +20,13 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Operation(summary = "查询一个用户")
     @GetMapping("/one")
     public Mono<User> getOneUser() {
         return userService.getOneUser();
     }
 
+    @Operation(summary = "查询所有用户")
     @GetMapping("/all")
     public Flux<User> getAllUser() {
         return userService.getAllUser();
